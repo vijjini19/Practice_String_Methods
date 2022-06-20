75e58b3c766a64bcc5212f47955784ed0a6058ad
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-06-20 20:47:14 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,349 @@
+# a="i am good"
+# print(a.capitalize())
+
+# b="PYTHON PROGRAMMING is GOOD"
+# print(b.casefold())
+
+# c="mustard"
+# print(c.center(13,'0'))
+
+# d="ssshbjkdjksjksssnsjsnj"
+# print(d.count("s"))
+
+# e="banana"
+# print(e.encode())
+
+# f="pythpn3"
+# print(f.endswith("3"))
+
+# g="a\tp\tp\tl\te"
+# print(g.expandtabs())
+
+# h="get well soon ok"
+# print(h.index("k"))
+
+# i="i am having bank balance of {price} rupees"
+# print(i.format(price=20000))
+
+# j="678bh8y7d"
+# print(j.isalnum())
+
+# k="gvhjhbblj"
+# print(k.isalpha())
+
+# l="008"
+# print(l.isdecimal())
+
+# p="hello world"
+# print(p.title())
+
+# q='hello','hai','hey'
+# print('sindhu'.join(q))
+
+# p="hello"
+# print(p.ljust(10,"0"))
+
+# a="welcome"
+# print(a.rjust(19,"0"))
+
+# j="jihhjk"
+# print(j.zfill(7))
+
+# k="huhljkklkl"
+# print(k.upper())
+
+# x="hello"
+# y="hai"
+# print(x.translate('y'))
+
+# y="hello,hai,how"
+# print(y.split())
+
+# print("hey python".capitalize())
+# print(" hi python".title())
+# print("Hey Python".istitle())
+# print("hey python".upper())
+# print("hey python".isupper())
+# print("Hey Python".lower())
+# print("hey python".islower())
+# a="hey python"
+# print(a.replace("hey",'hai'))
+# print(a.replace("python",'3'))
+# print("welcccome".count('c'))
+# print(len("python"))
+# print(len("welcome"))
+# z="hai"
+# print(len(z))
+p="sindhu"
+# print(p[0])
+# print(p[1])
+# print(p[2])
+# print(p[3])
+# print(p[4])
+# print(p[5])
+# print(p[-1])
+# print(p[-2])
+# print(p[-3])
+# print(p[-4])
+# print(p[-5])
+# print(p[-6])
+# print(p[0]+p[4])
+# u="pythonn"
+# print('python'.find('p'))
+# print('python'.find('t'))
+# print('python'.find('r'))
+# print('python'.index('n'))
+# a="   python   "
+# print(a.strip())
+# print(a.lstrip())
+# print(a.rstrip())
+# a=input()
+# print(type(a))
+# a=input('enter the first string')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
